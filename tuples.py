5404e241ddf9b6a7c0675f563b8953fc2f210c64
Pooja Patel <31209808+patelpooja1998@users.noreply.github.com>
2023-02-24 23:39:36 -0500

    Add tuples.py file

---
+++ tuples.py
@@ -0,0 +1,9 @@
+example_tuple = (1,2,3,4,5)
+print(example_tuple, type(example_tuple))
+print(example_tuple[1:2])
+
+personal_information = ('Pooja',24, 'Canada' )
+print(personal_information)
+
+merge_tuple = example_tuple + personal_information
+print(merge_tuple)
