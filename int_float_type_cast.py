720fded85c6a5f177cd28487670aeed6db7d4772
Pooja Patel <31209808+patelpooja1998@users.noreply.github.com>
2023-02-24 22:47:13 -0500

    Integers, Float and Type Casting in Python

---
+++ int_float_type_cast.py
@@ -0,0 +1,35 @@
+# Integers
+
+integer_one = 1
+integer_two = 2
+print(integer_one)
+print(integer_two)
+
+#operations on Integers
+print(integer_one + integer_two)
+print(integer_one * integer_two)
+print(integer_one - integer_two)
+print(integer_one / integer_two)
+
+# Integers as string
+string_one = '1'
+string_two = '2'
+print(string_one+string_two)
+
+print(type(integer_one), type(string_one))
+
+#Floats
+
+float_one = 1.0
+float_two = 2.0
+
+print(float_one)
+print(float_two)
+print(float_one + float_two)
+print(float_one - float_two)
+print(float_one * float_two)
+print(float_one / float_two)
+
+# Type casting
+print(integer_one + int(string_two))
+print(int(float_one * float_two))
