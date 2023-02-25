478619da8039b4548192b3278fc3c172596bc735
Pooja Patel <31209808+patelpooja1998@users.noreply.github.com>
2023-02-24 22:56:25 -0500

    Booleans in Python

---
+++ boolean.py
@@ -0,0 +1,15 @@
+# Boolean in Python
+
+boolean_true = True
+boolean_false = False
+
+print(boolean_true, boolean_false, type(boolean_true), type(boolean_false))
+
+# Boolean Operations
+var_one = 1
+var_two = 2
+
+print(var_one == var_two)
+print(var_one != var_two)
+print(var_one >= var_two)
+print(var_one <= var_two)
