31fc7259bddfde13f8abba453a7a055f18c0a164
Pooja Patel <31209808+patelpooja1998@users.noreply.github.com>
2023-02-26 12:47:40 -0500

    Format strings in python

---
+++ format_strings.py
@@ -0,0 +1,14 @@
+name = 'Pooja'
+
+Age = 24
+
+zip_code = 1234
+
+print('name is', name , 'and Age is ', Age, "lives at", zip_code)
+
+print(f"name:{name} age:{Age} zip_Code: {zip_code:05d}")
+
+outout_txt = f"name:{name} age:{Age}"
+
+print(outout_txt)
+
