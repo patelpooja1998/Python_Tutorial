8165d3305e12b14fdb2f04d492b5752da2e305d4
Pooja Patel <31209808+patelpooja1998@users.noreply.github.com>
2023-02-26 12:18:50 -0500

    Dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,7 @@
+example_dict = {'key':'value', 'key2':'value2', 'key3':'value3',
+                2: 3, 4:5,
+                'Name':'Pooja', 'Age': 24, 'Country':'Canada'}
+print(example_dict, type(example_dict))
+print(example_dict['key'])
+print(example_dict['Name'])
+
